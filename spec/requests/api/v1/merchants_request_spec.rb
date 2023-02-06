03a65e1d1d6a9bdf102fd6d800805092312a8973
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-06 15:28:04 -0800

    feat: setup database and initial gems

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "merchants API" do
+  it "sends a list of merchants" do
+    create_list(:merchant, 10)
+
+    get "/api/v1/merchants"
+
+    expect(response).to be_successful
+  end
+
+
+end
