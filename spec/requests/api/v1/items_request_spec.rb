5e1f04550b225bca7d5d3d48cf6604ab37c067f3
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-06 19:10:20 -0800

    test: wrote test for item index call

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "items API" do
+  it "sends a list of items" do
+    create_list(:item, 10)
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    items_data = JSON.parse(response.body, symbolize_names: true)
+    expect(items_data[:data].count).to eq(10)
+
+    items_data[:data].each do |item|
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+end
