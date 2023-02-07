10cc26538b46536ce42598ba62a425172c678ba4
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-06 16:35:23 -0800

    feat: implemented find all items for a given merchant endpoint

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.sentence }
+    unit_price { Faker::Number.between(from: 0.1, to: 5.0) }
+  end
+end
