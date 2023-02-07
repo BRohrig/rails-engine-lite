14c1d4375151cd7c2932cb7bf2000b0bc448a153
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-06 19:01:16 -0800

    fix: got merchant items to display 404 when merchant not found

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    @items = MerchantItemsFacade.find_all_by_merch_id(params[:merchant_id])
-    if @items.class == Error
-      render json: ErrorSerializer.new(@items).serialized_error
-    else
-      render json: ItemSerializer.new(@items)
-    end
+    render json: ItemSerializer.new(MerchantItemsFacade.find_all_by_merch_id(params[:merchant_id]))
   end
 
 end
--- app/facades/merchant_items_facade.rb
+++ app/facades/merchant_items_facade.rb
@@ -1,12 +1,5 @@
 class MerchantItemsFacade
   def self.find_all_by_merch_id(merch_id)
-    if !Item.find_by_merch_id(merch_id).empty?
-      Item.find_by_merch_id(merch_id)
-    else
-      Error.new("No Items Found for This Merchant", "Not Found", 404)
-    end
+    Merchant.find(merch_id).items
   end
-
-
-
 end
