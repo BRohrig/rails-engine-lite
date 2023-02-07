c56ba2c69c9098117c79c4b0cb52ee5bac97be28
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-06 19:18:32 -0800

    test: wrote test for item show endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "items API" do
-  it "sends a list of items" do
+  before(:each) do
     @merchant = create(:merchant)
+  end
+
+  it "sends a list of items" do
     create_list(:item, 10, merchant_id: @merchant.id)
 
     get "/api/v1/items"
@@ -22,5 +25,24 @@ RSpec.describe "items API" do
       expect(item[:attributes]).to have_key(:merchant_id)
       expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
+  end
+
+  it "can find a single item by its ID" do
+    id = create(:item, merchant_id: @merchant.id).id
+
+    get "/api/v1/items/#{id}"
+
+    item_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item_data[:data][:attributes]).to have_key(:name)
+    expect(item_data[:data][:attributes][:name]).to be_a(String)
+    expect(item_data[:data][:attributes]).to have_key(:description)
+    expect(item_data[:data][:attributes][:description]).to be_a(String)
+    expect(item_data[:data][:attributes]).to have_key(:unit_price)
+    expect(item_data[:data][:attributes][:unit_price]).to be_a(Float)
+    expect(item_data[:data][:attributes]).to have_key(:merchant_id)
+    expect(item_data[:data][:attributes][:merchant_id]).to eq(id.to_s)
   end
 end
