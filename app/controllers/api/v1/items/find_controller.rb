593c7c018328b36749b83679726cae082d2cafb1
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-09 15:17:03 -0800

    refactor: cleaned up logic for item find_all

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -3,15 +3,9 @@ module Api
     module Items
       class FindController < ApplicationController
         def find_all
-          if params[:name] != '' && !params[:name].nil? && !params[:min_price] && !params[:max_price]
-            render json: ItemSerializer.new(Item.find_by_name_fragment(params[:name]))
-          elsif params[:min_price].to_f.positive? && params[:max_price].to_f.positive? && !params[:name]
-            render json: ItemSerializer.new(Item.find_by_price(min_price: params[:min_price],
-                                                               max_price: params[:max_price]))
-          elsif params[:min_price].to_f.positive? && !params[:name]
-            render json: ItemSerializer.new(Item.find_by_price(min_price: params[:min_price]))
-          elsif params[:max_price].to_f.positive? && !params[:name]
-            render json: ItemSerializer.new(Item.find_by_price(max_price: params[:max_price]))
+          result = Item.search(name: params[:name], min_price: params[:min_price], max_price: params[:max_price])
+          if result
+            render json: ItemSerializer.new(result)
           else
             render json: { errors: 'Please Input Valid Parameter' }, status: 400
           end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,25 @@ class Item < ApplicationRecord
     end
   end
 
+  def self.search(name: nil, min_price: nil, max_price: nil)
+    if name != '' && !name.nil? && min_price.nil? && max_price.nil?
+      Item.find_by_name_fragment(name)
+    elsif (min_price.to_f.positive? || max_price.to_f.positive?) && name.nil?
+      Item.find_by_price(min_price: min_price, max_price: max_price)
+    else
+      false
+    end
+  end
+
+  private 
+
   def self.find_by_name_fragment(fragment)
     Item.where('name ilike ?', "%#{fragment}%").order(:name)
   end
 
-  def self.find_by_price(min_price: 0, max_price: 9_999_999)
-    Item.where("unit_price BETWEEN #{min_price} AND #{max_price}")
+  def self.find_by_price(min_price: nil, max_price: nil)
+    !min_price.nil? ? @min_price = min_price : @min_price = 0
+    !max_price.nil? ? @max_price = max_price : @max_price = 9999999
+    Item.where("unit_price BETWEEN #{@min_price} AND #{@max_price}")
   end
 end
